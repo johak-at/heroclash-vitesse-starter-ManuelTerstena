3095fb72dbf4e4c4b1c6d5837c5a6c487a0ee0c3
ManuelTerstena <95612307+ManuelTerstena@users.noreply.github.com>
2022-11-18 09:50:03 +0100

    bibabum

--- src/components/HeroCard/HeroStats.vue
+++ src/components/HeroCard/HeroStats.vue
@@ -3,11 +3,26 @@ const props = defineProps(['name', 'stats', 'alignment'])
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
+  <div :class="alignment">
+    <h1>{{ name }}</h1>
 
   <!-- iteriere mit v-for über die stats -->
+  <StatField v-for="(value,key) in stats" :key = "key" :skill="key" :value="value" />
+  </div>
 </template>
 
-<style>
- *{border: 1px solid red}
+<style scoped> 
+ 
+
+ .bad{
+ background-image: linear-gradient(135deg, #f12711, #f5af19);
+ }
+
+ .good{
+  background-image: linear-gradient(135deg, #007bb0, #9dc93d);
+ }
+
+ .neutral{
+  background-image: linear-gradient(135deg, #51f111, #f5af19);
+ }
 </style>
---
+++ src/components/HeroCard/StatField.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+const props = defineProps(['skill', 'value']);
+
+const icon = computed (() => {
+
+    switch(props.skill){
+
+        case 'intelligence':
+
+            return 'i-game-icons-brains';
+
+        case 'strength':
+
+            return 'dumbbell';
+
+        case 'speed':
+
+            return 'running';
+
+        case 'durability':
+
+            return '';
+
+        case 'power':
+
+            return 'i-game-icons-fist';
+
+        case 'combat':
+
+            return 'i-game-icons-crossed-swords';
+
+        default:
+
+            return 'question';
+
+    }
+
+})
+
+</script>
+
+
+
+<template>
+
+    <div flex justify-between>
+
+        <span> <i class="i-game-icons-crossed-swords" inline-block ></i>{{skill[0].toUpperCase() +skill.substring(1, skill.length)}}</span>
+
+        <span>{{value}}</span>
+
+    </div>
+
+</template>
